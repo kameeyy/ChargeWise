2c9122078854353f5c1444f7fd7e014f079a98e0
DanielTimms <108647495+DanielTimms@users.noreply.github.com>
2025-11-08 00:05:18 +0800

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -265,7 +265,7 @@ with tab1:
     col1.metric("Avg Charging Duration", f"{int((m:=filtered_df['Unscaled_Charging_Duration'].mean()))}m {int(round((m % 1)*60))}s") #Changes by Daniel
     #col1.metric("Avg Charging Duration", f"{filtered_df['Unscaled_Charging_Duration'].mean():.2f} min")
     col2.metric("Avg Operating Temp", f"{filtered_df['Battery_Operating_Temperature'].mean():.2f} °C")
-    col3.metric("Avg Energy Consumed", lambda avg_energy: (f"{avg_energy * 1000:.3f} mWh" if avg_energy < 0.001 else f"{avg_energy / 1000:.3f} kWh" if avg_energy >= 1000 else f"{avg_energy:.3f} Wh") )(filtered_df['Total_Energy_Consumed'].mean())) #Changes by Daniel
+    col3.metric("Avg Energy Consumed", (lambda x: f"{x * 1000:.3f} mWh" if x < 0.001 else f"{x / 1000:.3f} kWh" if x >= 1000 else f"{x:.3f} Wh")(filtered_df['Total_Energy_Consumed'].mean())) #Changes by Daniel
     #col3.metric("Avg Energy Consumed", f"{filtered_df['Total_Energy_Consumed'].mean():.4f} Wh")
     col4.metric("Predicted Capacity", f"{filtered_df['Predicted_Capacity'].mean():.2f} %")
 
